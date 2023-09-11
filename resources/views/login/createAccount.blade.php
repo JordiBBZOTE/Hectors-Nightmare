d72ef3f378f5db03c5df0a080e2563d5d3a50123
JORGE MARQUEZ JIMENEZ <jorgoniovib@gmail.com>
2023-09-11 15:13:41 -0600

    fix

--- resources/views/login/createAccount.blade.php
+++ resources/views/login/createAccount.blade.php
@@ -20,8 +20,14 @@
         <input class="recuadros" type="password" placeholder="Ingrese su contraseña" required name="password"><br>
         
         <br><label class="labels" for="imguser">Imagen</label><br>
-        <input class="recuadros" type="text" placeholder="Ingresa tu(s) imagen(s)" autocomplete="off" name="imguser" required><br>
-
+        
+        <input type="radio" id="html" name="imguser" value="1">
+        <label for="1">1</label><br>
+        <input type="radio" id="css" name="imguser" value="2">
+        <label for="2">2</label><br>
+        <input type="radio" id="javascript" name="imguser" value="3">
+        <label for="3">3</label>
+        <br>
         <br><input  class="Registrar"  type="submit" value="Crear">
     </form>
 </div>
